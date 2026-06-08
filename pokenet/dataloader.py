65b86ac70f949f72d28a0b730c37b2855eca9686
Mandy Zhan <mzhan@n0163.talapas.uoregon.edu>
2026-06-08 03:34:58 -0700

    Added shared species_to_index mapping

--- pokenet/dataloader.py
+++ pokenet/dataloader.py
@@ -14,7 +14,7 @@ class PokemonDataset(Dataset):
     The dataset structure is based on images organized in folders by label
     '''
 
-    def __init__(self, root_directory, transform = None):
+    def __init__(self, root_directory, transform = None, species_to_index = None):
         '''
         Args:
         root_directory: path to train/, val/, or test/ folders
@@ -23,16 +23,19 @@ class PokemonDataset(Dataset):
         self.root_directory = Path(root_directory)
         self.transform = transform
         self.samples = []  # Initialize an empty list to hold the labels and corresponding images
-        self.species_to_index = {}
+        self.species_to_index = species_to_index or {}
 
         # Iterate through each folder and find the corresponding labels
         for label_index, species_directory in enumerate(sorted(self.root_directory.iterdir())):
             if species_directory.is_dir():
                 species_name = species_directory.name
-                self.species_to_index[species_name] = label_index
-                for image_file in species_directory.iterdir():
-                    if image_file.suffix.lower() in image_extensions:
-                        self.samples.append((image_file, label_index))
+                if species_to_index is None:
+                    self.species_to_index[species_name] = label_index
+                index = self.species_to_index.get(species_name)
+                if index is not None:
+                    for image_file in species_directory.iterdir():
+                        if image_file.suffix.lower() in image_extensions:
+                            self.samples.append((image_file, index))
 
     def __len__(self):
         '''
@@ -78,15 +81,19 @@ def get_data_loaders(base_path, batch_size = 64, image_size = 256):
     batch_size (int): Batch size for the dataloaders
     '''
     transform = data_transform(image_size)
+    train_path = os.path.join(base_path, "train")
+    shared_species_to_index = {
+        species.name: index for index, species in enumerate(sorted(Path(train_path).iterdir())) if species.is_dir()
+    }
 
     # Apply transformations to the dataset from each directory
-    train_dataset = PokemonDataset(os.path.join(base_path, "train"), transform = transform)
-    validation_dataset = PokemonDataset(os.path.join(base_path, "val"), transform = transform)
-    test_dataset = PokemonDataset(os.path.join(base_path, "test"), transform = transform)
+    train_dataset = PokemonDataset(os.path.join(base_path, "train"), transform = transform, species_to_index = shared_species_to_index)
+    validation_dataset = PokemonDataset(os.path.join(base_path, "val"), transform = transform, species_to_index = shared_species_to_index)
+    test_dataset = PokemonDataset(os.path.join(base_path, "test"), transform = transform, species_to_index = shared_species_to_index)
 
     # Get a batch of data of training data
-    train_loader = DataLoader(dataset = train_dataset, batch_size = 64, shuffle = True)
-    validation_loader = DataLoader(dataset = validation_dataset, batch_size = 64)
-    test_loader = DataLoader(dataset = test_dataset, batch_size = 64)
+    train_loader = DataLoader(dataset = train_dataset, batch_size = batch_size, shuffle = True)
+    validation_loader = DataLoader(dataset = validation_dataset, batch_size = batch_size)
+    test_loader = DataLoader(dataset = test_dataset, batch_size = batch_size)
 
     return train_loader, validation_loader, test_loader
